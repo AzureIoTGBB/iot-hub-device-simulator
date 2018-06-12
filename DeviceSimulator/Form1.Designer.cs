2789401da3f6116d6f0064121c332c24676b478e
Danilo Diaz <ddiaz@live.com>
2018-06-12 16:24:55 -0400

    Initial commit

---
+++ DeviceSimulator/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace DeviceSimulator
+{
+    partial class DeviceSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbPayload = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgMessageProp = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtDesired = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgReported = new System.Windows.Forms.DataGridView();
+            this.NameCell = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMessageProp)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReported)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // rtbPayload
+            // 
+            this.rtbPayload.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbPayload.Location = new System.Drawing.Point(9, 171);
+            this.rtbPayload.Name = "rtbPayload";
+            this.rtbPayload.Size = new System.Drawing.Size(312, 83);
+            this.rtbPayload.TabIndex = 2;
+            this.rtbPayload.Text = "{\"temperature\":\"%%%\", \"humidity\":\"%%%\"}";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Payload";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dgMessageProp);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtInterval);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.rtbPayload);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 296);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Telemetry";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Message Properties";
+            // 
+            // dgMessageProp
+            // 
+            this.dgMessageProp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgMessageProp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMessageProp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dgMessageProp.Location = new System.Drawing.Point(9, 32);
+            this.dgMessageProp.Name = "dgMessageProp";
+            this.dgMessageProp.Size = new System.Drawing.Size(312, 120);
+            this.dgMessageProp.TabIndex = 9;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 130;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Value";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 130;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Interval";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtInterval.Location = new System.Drawing.Point(9, 273);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(85, 20);
+            this.txtInterval.TabIndex = 7;
+            this.txtInterval.Text = "5";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(246, 262);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(596, 20);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 28);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Connect";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtDesired);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.dgReported);
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(345, 51);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 296);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Twin";
+            // 
+            // txtDesired
+            // 
+            this.txtDesired.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDesired.Location = new System.Drawing.Point(8, 171);
+            this.txtDesired.Name = "txtDesired";
+            this.txtDesired.ReadOnly = true;
+            this.txtDesired.Size = new System.Drawing.Size(312, 83);
+            this.txtDesired.TabIndex = 10;
+            this.txtDesired.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Desired Properties (Read Only)";
+            // 
+            // dgReported
+            // 
+            this.dgReported.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgReported.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgReported.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameCell,
+            this.Value});
+            this.dgReported.Location = new System.Drawing.Point(9, 32);
+            this.dgReported.Name = "dgReported";
+            this.dgReported.Size = new System.Drawing.Size(311, 120);
+            this.dgReported.TabIndex = 8;
+            // 
+            // NameCell
+            // 
+            this.NameCell.HeaderText = "Name";
+            this.NameCell.Name = "NameCell";
+            this.NameCell.Width = 130;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.Width = 130;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(245, 262);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 28);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Submit";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Reported Properties";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.txtStatus);
+            this.groupBox3.Location = new System.Drawing.Point(12, 353);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(659, 222);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Status";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(6, 17);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(647, 199);
+            this.txtStatus.TabIndex = 0;
+            this.txtStatus.Text = "";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConnectionString.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DeviceSimulator.Properties.Settings.Default, "ConnectionString", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtConnectionString.Location = new System.Drawing.Point(12, 25);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(578, 20);
+            this.txtConnectionString.TabIndex = 0;
+            this.txtConnectionString.Text = global::DeviceSimulator.Properties.Settings.Default.ConnectionString;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 578);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(684, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(79, 17);
+            this.toolStripStatusLabel1.Text = "Disconnected";
+            // 
+            // DeviceSimulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 600);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConnectionString);
+            this.MaximizeBox = false;
+            this.Name = "DeviceSimulator";
+            this.Text = "Device Simulator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMessageProp)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReported)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbPayload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox txtStatus;
+        private System.Windows.Forms.DataGridView dgReported;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgMessageProp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameCell;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.RichTextBox txtDesired;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ DeviceSimulator/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+
+namespace DeviceSimulator
+{
+    public partial class DeviceSimulator : Form
+    {
+        public string ConnectionString { get; set; }
+
+        int telemetryInterval = 1;
+        private static DeviceClient deviceClient;
+        bool Loop = true;
+
+        public DeviceSimulator()
+        {
+            InitializeComponent();
+            
+            ConnectionString = txtConnectionString.Text;
+            try
+            {
+                telemetryInterval = int.Parse(txtInterval.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Interval must be a interger");
+            }
+        }
+
+        private void ConnectionChange(ConnectionStatus status, ConnectionStatusChangeReason reason)
+        {
+            toolStripStatusLabel1.Text = status.ToString();
+
+            if (status == ConnectionStatus.Connected)
+            {
+                btnStart.Invoke((MethodInvoker)delegate
+                {
+                    btnStart.Enabled = true;
+                });
+
+                btnSend.Invoke((MethodInvoker)delegate
+                {
+                    btnSend.Enabled = true;
+                });
+            }               
+            else
+            {
+                btnStart.Invoke((MethodInvoker)delegate
+                {
+                    btnStart.Enabled = false;
+                });
+
+                btnSend.Invoke((MethodInvoker)delegate
+                {
+                    btnSend.Enabled = false;
+                });
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ConnectionString = txtConnectionString.Text;
+
+            if (btnUpdate.Text == "Connect")
+            {
+                Connect();
+            }
+            else
+                Disconnect();
+        }
+        private void Connect()
+        {
+            try
+            {
+                deviceClient = DeviceClient.CreateFromConnectionString(ConnectionString, TransportType.Mqtt);
+                deviceClient.SetConnectionStatusChangesHandler(ConnectionChange);
+                deviceClient.SetMethodHandlerAsync("SetInterval", DirectMethodResponse, null);
+                deviceClient.SetDesiredPropertyUpdateCallbackAsync(TwinResponse, null);
+               
+                btnUpdate.Text = "Disconnect";                
+                statusStrip1.Refresh();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not able to connect. Please check your connection string and try again");
+            }
+        }
+        private void Disconnect()
+        {
+            deviceClient.CloseAsync();
+            btnUpdate.Text = "Connect";            
+            statusStrip1.Refresh();
+        }
+        private Task TwinResponse(TwinCollection desiredProperties, object userContext)
+        {
+            string result = "{\"result\":\"Executed twin update\"}";
+            
+            txtStatus.Invoke((MethodInvoker)delegate {
+                txtDesired.AppendText("Twin updaded: " + desiredProperties.ToJson() + Environment.NewLine);   
+            });
+            return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+        }
+        private Task<MethodResponse> DirectMethodResponse(MethodRequest methodRequest, object userContext)
+        {
+            string result = "{\"result\":\"Executed direct method: " + methodRequest.Name + "\"}";
+            bool error = false;
+            var json = JsonConvert.DeserializeObject<string>(methodRequest.DataAsJson);
+                       
+            try
+            {
+                telemetryInterval = int.Parse(json);
+            }
+            catch (Exception)
+            {
+                error = true;
+            }
+
+            txtInterval.Invoke((MethodInvoker)delegate
+            {
+                txtInterval.Text = json;
+            });
+
+            txtStatus.Invoke((MethodInvoker)delegate {
+                if(!error)
+                    txtStatus.AppendText("Method called, interval updated"+ Environment.NewLine);
+                else
+                    txtStatus.AppendText("Method called but interval could not be updated" + Environment.NewLine);
+
+            });
+            return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+        }
+        private string GetPayload()
+        {
+            string results = "";
+            Random rand = new Random();
+
+            double r = 40 + rand.NextDouble() * 20;
+            results = rtbPayload.Text.Replace("%%%", r.ToString());
+
+            return results;
+        }
+        private async void SendReportedPropertiesAsync()
+        {
+            TwinCollection reportedProperties;
+            reportedProperties = new TwinCollection();
+            foreach (DataGridViewRow row in dgReported.Rows)
+            {
+                if (row.Cells[0].Value != null)
+                {
+                    string Name = row.Cells[0].Value.ToString();
+                    string Value = row.Cells[1].Value.ToString();
+                    reportedProperties[Name] = Value;
+                }
+            }
+            await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+        private async void SendDeviceToCloudMessagesAsync()
+        {
+            Random rand = new Random();
+
+            while (Loop)
+            {
+                var messageString = JsonConvert.SerializeObject(GetPayload());
+                var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(messageString));
+
+                foreach (DataGridViewRow row in dgMessageProp.Rows)
+                {
+                    if (row.Cells[0].Value != null)
+                    {
+                        string Name = row.Cells[0].Value.ToString();
+                        string Value = row.Cells[1].Value.ToString();
+                        message.Properties.Add(Name, Value);
+                    }
+                }
+                // Send the tlemetry message
+                await deviceClient.SendEventAsync(message);
+                txtStatus.AppendText(string.Format("{0} > Sending message: {1}", DateTime.Now, messageString) + Environment.NewLine);
+
+                await Task.Delay(telemetryInterval * 1000);
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            SendDeviceToCloudMessagesAsync();
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (Loop)
+            {
+                btnSend.Text = "Stop";
+                Loop = true;
+            }
+            else
+            {
+                btnSend.Text = "Start";
+                Loop = false;
+            }
+            SendReportedPropertiesAsync();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}   
+
